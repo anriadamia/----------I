faab74088882e09d7ffb8b43bb46fe38a3cee39f
anriadamia <anriadamia2000@gmail.com>
2022-12-28 10:46:39 +0400

    დავალება I

---
+++ scripts.js
@@ -0,0 +1,27 @@
+// სავარჯიშო I
+// const numbers=[1,4,7,-2,-9,3,9,10];
+// const positiveNumbers=numbers.filter((nums)=>nums>0);
+// console.log(positiveNumbers);
+// const sumOfPositiveNumbers=positiveNumbers.reduce((total, currentValue)=>{
+//     return total+currentValue
+// },0)
+// console.log(sumOfPositiveNumbers);
+
+// სავარჯიშო IV
+// const addAsync=(par1,par2)=>{
+//    return new Promise((resolve,reject)=>{
+//         if(par1,par2!==undefined  && typeof par1=="number" && typeof par2=="number"){
+//             resolve(par1+par2);
+//         }else {
+//             reject("error");
+//         }
+//     })
+// }
+// addAsync(2,4);
+// addAsync(2,0);
+// addAsync(2,"a")
+// .then((val)=>console.log(val))
+// .catch((error)=>console.log(error))
+
+
+
